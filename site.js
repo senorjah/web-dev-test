410abbbc3bba6f59c9791efe56c5413831e76f98
senorjah <98075415+senorjah@users.noreply.github.com>
2023-05-03 15:52:19 -0700

    final draft
    website final draft, all content shown

--- site.js
+++ site.js
@@ -1,15 +1,14 @@
 var x = 0;
 
+window.onscroll = function() {stick()};
 
-window.onscroll = function() {sticky()};
-
-var navbar = document.getElementById("navbar");
+var navbar = document.querySelector('#navbar');
 
 var sticky  = navbar.offsetTop;
 
-function myFunction() {
+function stick() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+        navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
     }
